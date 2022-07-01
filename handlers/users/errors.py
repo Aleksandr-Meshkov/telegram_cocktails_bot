752b773e162f8737638889220ab082e7e83358e1
Александр Мешков <meshkov.soner1989@yandex.ru>
2022-07-02 01:48:07 +0300

    corrections

--- handlers/users/errors.py
+++ handlers/users/errors.py
@@ -1,12 +1,14 @@
 from aiogram import types
 
 from loader import dp
+from keybords.default import kb_choice
 
 
 @dp.message_handler()
 async def catch_some_text(message: types.Message):
     await message.answer(
-        'Упс, такой команды нет, но возможно она появится потом!'
+        'Упс, такой команды нет, но возможно она появится потом!',
+        reply_markup=kb_choice
     )
 
 
@@ -14,5 +16,6 @@ async def catch_some_text(message: types.Message):
 async def cath_some_not_text(message: types.Message):
     await message.answer(
         'Ух, классно, но я пока что не умею отвечать правильно\n'
-        f'на такие сообщения: <b>{message.content_type}</b>'
-        )
+        f'на такие сообщения: <b>{message.content_type}</b>',
+        reply_markup=kb_choice
+    )
